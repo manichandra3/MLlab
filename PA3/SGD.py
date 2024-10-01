8c349734875135acf026b99b5a78e51ae97a288d
Mani Chandra <127130821+manichandra3@users.noreply.github.com>
2024-10-01 22:19:14 +0530

    Too many changes

--- PA3/SGD.py
+++ PA3/SGD.py
@@ -41,11 +41,11 @@ def stochastic_gradient_descent(w, x, b, y, num_iterations=50, alpha=0.01):
             w_cost, b_cost = compute_gradient(w, x_shuffled[j], b, y_shuffled[j], alpha)
             w -= w_cost
             b -= b_cost
-        cost_history.append(cost_function(w, x, b, y))
+        cost_history.append(cost_function(w, x_shuffled, b, y_shuffled))
         b_history.append(b)
         w_history.append(w)
         if i % 10 == 0:
-            print(f"Iteration {i}: Cost = {cost_function(w, x, b, y)}")
+            print(f"Iteration {i}: Cost = {cost_function(w, x_shuffled, b, y_shuffled)}")
     print(f"{w,b}")
     print(f'Stochastic gradient descent ends')
     return w, b, cost_history, w_history, b_history
@@ -77,13 +77,6 @@ plt.show()
 plt.title("Cost Function Plot")
 plt.plot(cost_plt, label="Cost")
 plt.xlabel('Iteration')
-plt.ylabel('Cost')
-plt.legend()
-plt.show()
-
-plt.title("Cost vs. Weights Plot")
-plt.plot(w_plt, cost_plt, label="Cost vs. Weights")
-plt.xlabel('Weights')
 plt.ylabel('Cost')
 plt.legend()
 plt.show()
