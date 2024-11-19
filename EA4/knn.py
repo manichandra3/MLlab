1a9f6de405eebc4e15b25b9f5fc482b2bccae75c
Mani Chandra <127130821+manichandra3@users.noreply.github.com>
2024-11-19 09:08:42 +0530

    Formatted

--- EA4/knn.py
+++ EA4/knn.py
@@ -11,41 +11,46 @@ dataset.drop(columns=['Patient_ID'])
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 print(X_train)
 
+
 class KNN():
-  def __init__(self,k):
-    self.k=k
-    print(self.k)
-  def fit(self,X_train,y_train):
-    self.x_train=X_train
-    self.y_train=y_train
-  def calculate_euclidean(self,sample1,sample2):
-    distance=0.0
-    for i in range(len(sample1)):
-      distance+=(sample1[i]-sample2[i])**2
-    return sqrt(distance)
-  def nearest_neighbors(self,test_sample):
-    distances=[]
-    for i in range(len(self.x_train)):
-      distances.append((self.y_train[i],self.calculate_euclidean(self.x_train[i],test_sample)))
-    distances.sort(key=lambda x:x[1])
-    neighbors=[]
-    for i in range(self.k):
-      neighbors.append(distances[i][0])
-    return neighbors
-  def predict(self,test_set):
-    predictions=[]
-    for test_sample in test_set:
-      neighbors=self.nearest_neighbors(test_sample)
-      labels=[sample for sample in neighbors]
-      prediction=max(labels,key=labels.count)
-      predictions.append(prediction)
-    return predictions
+    def __init__(self, k):
+        self.k = k
+        print(self.k)
+
+    def fit(self, X_train, y_train):
+        self.x_train = X_train
+        self.y_train = y_train
+
+    def calculate_euclidean(self, sample1, sample2):
+        distance = 0.0
+        for i in range(len(sample1)):
+            distance += (sample1[i] - sample2[i]) ** 2
+        return sqrt(distance)
+
+    def nearest_neighbors(self, test_sample):
+        distances = []
+        for i in range(len(self.x_train)):
+            distances.append((self.y_train[i], self.calculate_euclidean(self.x_train[i], test_sample)))
+        distances.sort(key=lambda x: x[1])
+        neighbors = []
+        for i in range(self.k):
+            neighbors.append(distances[i][0])
+        return neighbors
+
+    def predict(self, test_set):
+        predictions = []
+        for test_sample in test_set:
+            neighbors = self.nearest_neighbors(test_sample)
+            labels = [sample for sample in neighbors]
+            prediction = max(labels, key=labels.count)
+            predictions.append(prediction)
+        return predictions
 
 
 def accuracy(y_true, y_pred):
@@ -53,7 +58,7 @@ def accuracy(y_true, y_pred):
     return correct / len(y_true)
 
 
-Is = [1,2,3,4,5,6,7,8,9,10]
+Is = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 accuracies = []
 for i in Is:
     model = KNN(i)
